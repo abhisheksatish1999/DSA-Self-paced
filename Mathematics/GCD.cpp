fe251238b801217155f18de6e5a80d3a4b70e5aa
Abhishek Satish <abhisheksatish10@gmail.com>
2021-07-25 18:13:09 +0530

    Euclidean naive method

--- Mathematics/GCD.cpp
+++ Mathematics/GCD.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+//Naive Soltn
+/*
 int main()
 {
     int x,y;
@@ -17,3 +19,26 @@ int main()
     cout<<"GCD = "<<res;
     return(0);
 }
+*/
+//Euclidean Soltn
+
+int gcd(int a,int b)
+{
+    while(a!=b)
+    {
+        if(a>b)
+        a=a-b;
+        else
+        b=b-a;
+    }
+    return(a);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+    return(0);
+}
+
