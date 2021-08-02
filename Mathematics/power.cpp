f2a62e45ac2992f14a0b99d3917f3f28ebd89b28
Abhishek Satish <abhisheksatish10@gmail.com>
2021-08-02 12:15:10 +0530

    SC - O(log(n))  TC- O(1)

--- Mathematics/power.cpp
+++ Mathematics/power.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+/*
 int power(int x,int y)
 {
     if(y==0)
@@ -10,6 +11,28 @@ int power(int x,int y)
     return(temp);
     else
     return(temp*x);
+}
+int main()
+{
+    int p,x;
+    cin>>p>>x;
+    cout<<power(p,x);
+    return(0);
+}
+
+*/
+
+int power(int x,int y)                              //pow(3,5)  101
+{
+    int res=1;
+    while(y>0)
+    {
+        if(y%2!=0)
+        res*=x;
+        x=x*x;
+        y=y/2;
+    }
+    return(res);
 }
 int main()
 {
